51e1f9070f3c7e40f5d31ae49406bb628cba3bdf
Crystal Craig <ckcraig@amarillocollege.com>
2025-04-17 14:02:19 -0500

    updated

--- 4_17_Practice/Program.cs
+++ 4_17_Practice/Program.cs
@@ -88,27 +88,111 @@ namespace _4_17_Practice
 
             //EX10 -- SIMPLE PASSWORD CHECKER
 
-            //DECLARATION
-            string Userpassword;
-            const string password = "secret";
+            ////DECLARATION
+            //string Userpassword;
+            //const string password = "secret";
 
-            //INPUT
-            Console.WriteLine("Enter your password:");
-            Userpassword = Console.ReadLine();
+            ////INPUT
+            //Console.WriteLine("Enter your password:");
+            //Userpassword = Console.ReadLine();
+
+            ////process
+            //if (Userpassword == password)
+
+            //{
+            //    Console.WriteLine("Successful");
+            //}
+            //else 
+            //{ 
+            //    Console.WriteLine("Try again."); 
+            //}
+
+            //int age;
+
+            //Console.WriteLine("what is your age");
+            //age = Convert.ToInt32(Console.ReadLine());
+            //bool CanDrinkAlcohol = false;
+            //if (age > 21) 
+            //    CanDrinkAlcohol |= true;
+            //Console.WriteLine(CanDrinkAlcohol);
+
+
+
+            ////EX1 -- Number Classification if its postive, negative or zero, & even or odd
+
+            ////delcaration
+            //int userInput;
+
+            ////input
+            //Console.WriteLine("Input a number:");
+            //userInput = Convert.ToInt32(Console.ReadLine());
+
+            ////process
+            //if (userInput == 0)
+            //{
+            //    Console.WriteLine("Your number is zero");
+            //}
+            //else if (userInput < 0)
+
+            //{
+            //    Console.WriteLine("Your number is negative");
+            //}
+            //else
+
+            //{
+            //    Console.WriteLine("Your number is postive");
+            //}
+            //if (userInput % 2 == 0)
+            //{
+            //    Console.WriteLine("It is an even number");
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("It is an odd number");
+            //}
+
+
+            //EX2 GRADE CONVERTER TAKE NUMERIC SCORE TO LETTER GRADE
+
+            //declaration
+            int numericGrade;
+            int convertGrade;
+            //input
+            Console.WriteLine("Enter your numeric grade:");
+            numericGrade = Convert.ToInt32(Console.ReadLine());
 
             //process
-            if (Userpassword == password)
-
-            {
-                Console.WriteLine("Successful");
-            }
-            else 
-            { 
-                Console.WriteLine("Try again."); 
-            }
-
+            convertGrade = numericGrade / 10;
             
-           
+            switch (convertGrade)
+            {
+                case 0:
+                case 1:
+                  
+                        break;
+                case 2:
+                    Console.WriteLine("A");
+                    break;
+
+                case 3:
+                    Console.WriteLine("B");
+                    break;
+
+                case 4:
+                    Console.WriteLine("C");
+                    break;
+
+                case 5:  
+                    Console.WriteLine("D");
+                    break;
+                default:
+                    Console.WriteLine("F");
+                    break;
+
+                   
+            }
+
 
 
 
